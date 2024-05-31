ea7a2f51ecd2c7848e9f61fe6868166110c980ac
Aadishx07 <74809669+Aadishx07@users.noreply.github.com>
2024-05-31 22:14:45 +0530

    add

--- rename.sh
+++ rename.sh
@@ -8,17 +8,16 @@ files=$(ls *.jpg *.png | sort)
 
 # Loop through the sorted list of files
 for file in $files; do
-  # Extract the file extension
-  extension="${file##*.}"
-  # Format new file name with leading zeros and keep the original extension
-  new=$(printf "%03d.$extension" "$counter")
-  # Only rename the file if the new name does not exist
-  if [ ! -e "$new" ]; then
-    mv -- "$file" "$new"
-  else
-    echo "File $new already exists. Skipping $file"
-  fi
-  # Increment the counter
-  ((counter++))
+	# Extract the file extension
+	extension="${file##*.}"
+	# Format new file name with leading zeros and keep the original extension
+	new=$(printf "%03d.$extension" "$counter")
+	# Only rename the file if the new name does not exist
+	if [ ! -e "$new" ]; then
+		mv -- "$file" "$new"
+	else
+		echo "File $new already exists. Skipping $file"
+	fi
+	# Increment the counter
+	((counter++))
 done
-
